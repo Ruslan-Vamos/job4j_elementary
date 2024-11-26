ee0b7f083dc1fd822ea92207f92660da939c0b62
Ruslan-Vamos <ruslan_yurich@mail.ru>
2024-11-27 01:20:46 +0300

    1.1.2.9.1. String [#237407 #31715]

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Out {
+
+    public static void main(String[] args) {
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+        String name = "Ruslan";
+        String surname = "Popov";
+        String separate = " ";
+        String id = name + separate + surname;
+        System.out.println(id);
+    }
+}
