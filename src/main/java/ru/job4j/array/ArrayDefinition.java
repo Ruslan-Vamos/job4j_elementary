e8deb810c3a982fe32cc5411a9a3d9b8ec8c73fd
Ruslan-Vamos <ruslan_yurich@mail.ru>
2024-12-09 19:13:36 +0300

    1.1.6.6.0.3. Заполнение массива. [#226300 #31700]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Dubin";
+        names[1] = "Nikolai Kurchatov";
+        names[2] = "Aleksei Prohorov";
+        names[3] = "Aleksandr Anisimov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
