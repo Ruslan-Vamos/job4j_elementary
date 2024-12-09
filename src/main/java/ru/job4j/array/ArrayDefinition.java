56aa5e8f46593bddc9de42b03409a5e4394fcfc3
Ruslan-Vamos <ruslan_yurich@mail.ru>
2024-12-09 22:55:59 +0300

    1.1.6.6.0.4. Массивы и цикл for. [#226665 #31699]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -14,10 +14,8 @@ public class ArrayDefinition {
         names[1] = "Nikolai Kurchatov";
         names[2] = "Aleksei Prohorov";
         names[3] = "Aleksandr Anisimov";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
-
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
